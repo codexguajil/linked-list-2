db52ad41e9c9401c9ad967329ed747d9cfb69be1
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2017-12-10 15:28:02 -0700

    Complete toggle of read class

--- script.js
+++ script.js
@@ -11,12 +11,20 @@ function bookmarkCreate(e) {
   e.preventDefault();
   bookmarkSection.prepend(`<article class="appended-bookmark"><h2>${bookmarkTitle.val()}</h2>
       <a href="${bookmarkUrl.val()}">${bookmarkUrl.val()}</a>
-      <button id="mark-as-read">Mark as Read</button>
+      <button id="mark-as-read">Unread</button>
       <button id="remove-bookmark">Delete</button></article>`)
 }
 
 function readButtonToggle(){
   console.log('hello') 
+  $( ".appended-bookmark" ).toggleClass( 'read');
+    if ( $( this ).parent().is( ".read" ) ) {
+      $("#mark-as-read").text('Unread');
+//     } else if{( $( this ).parent().is( ".read" ) ) {
+//       $("#mark-as-read").text('Unread');
+//       return "sad";
+    }
+
 }
 
 // $("#mark-as-read").click(function(){
