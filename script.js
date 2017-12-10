2eb247bc0ac1802d302746b30b5c4701bba55873
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2017-12-10 14:58:57 -0700

    Add read button responsiveness

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ var enterButton = $('.enter-button');
 var bookmarkTitle = $('#bookmark-title');
 var bookmarkUrl = $('#bookmark-url');
 var bookmarkSection = $('.section-right');
+// var markAsReadButton = $('#mark-as-read');
 
 enterButton.click(bookmarkCreate);
+$(bookmarkSection).on('click','#mark-as-read', readButtonToggle);
 
 function bookmarkCreate(e) {
   e.preventDefault();
-  bookmarkTitle.val();
-  bookmarkUrl.val();
-  bookmarkSection.prepend(`<h2>${bookmarkTitle.val()}</h2>
+  bookmarkSection.prepend(`<article class="appended-bookmark"><h2>${bookmarkTitle.val()}</h2>
       <a href="${bookmarkUrl.val()}">${bookmarkUrl.val()}</a>
       <button id="mark-as-read">Mark as Read</button>
-      <button id="remove-bookmark">Remove</button>`)
+      <button id="remove-bookmark">Delete</button></article>`)
 }
+
+function readButtonToggle(){
+  console.log('hello') 
+}
+
+// $("#mark-as-read").click(function(){
+// //     $(".appended-bookmark").toggleClass("read");
+//     console.log('someshit');
+// });
