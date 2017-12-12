facc1a9bf843d87cefd45655e610fe3fdf90c62a
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2017-12-11 17:56:49 -0700

    Add CSS stylings for desktop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var enterButton = $('.enter-button');
-var bookmarkTitle = $('#bookmark-title');
-var bookmarkUrl = $('#bookmark-url');
-var bookmarkSection = $('.section-right');
+var enterButton = $('.js-submit-bookmark');
+var bookmarkTitle = $('.js-type-title');
+var bookmarkUrl = $('.js-type-url');
+var bookmarkSection = $('.js-bookmark-section');
 
 enterButton.click(bookmarkCreate);
 $(bookmarkSection).on('click','#mark-as-read', readButtonToggle);
@@ -11,17 +11,15 @@ function bookmarkCreate(e) {
   e.preventDefault();
   bookmarkSection.prepend(`<article class="appended-bookmark"><h2>${bookmarkTitle.val()}</h2>
       <a href="${bookmarkUrl.val()}">${bookmarkUrl.val()}</a>
-      <button id="mark-as-read">Unread</button>
+      <button id="mark-as-read">Read</button>
       <button id="remove-bookmark">Delete</button></article>`)
 }
 
 function readButtonToggle(){
   $(this).parent().toggleClass('read');
-  
 }
 
 function bookmarkDelete(){
   $(this).parent().remove();
-
 }
 
